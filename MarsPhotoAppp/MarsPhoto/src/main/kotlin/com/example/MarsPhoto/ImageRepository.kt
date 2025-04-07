6a909e8828e3964d1c25eccaa32f0e1d693fa6a3
DongThi0410 <thivhd.23ite@vku.udn.vn>
2025-04-08 00:06:56 +0700

    del sth

--- MarsPhotoAppp/MarsPhoto/src/main/kotlin/com/example/MarsPhoto/ImageRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.MarsPhoto
-
-import com.example.MarsPhoto.Image
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface ImageRepository : JpaRepository<Image, Int>
--- MarsPhotoAppp/MarsPhotoApp/app/src/main/java/com/example/marsphotoapp/dto/Image.kt
+++
@@ -1,7 +0,0 @@
-package com.example.marsphotoapp.dto
-
-data class Image (
-    val id: Int = 0,
-    val image_url: String = "",
-    val des: String = ""
-)
